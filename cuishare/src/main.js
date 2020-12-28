de87e74d8d9f31956e6964366713b7af8b6a0ab5
jiang6145 <jiang.notes14@gmail.com>
2020-12-28 16:33:30 +0800

    update

--- cuishare/src/main.js
+++ cuishare/src/main.js
@@ -2,11 +2,17 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
+import axios from 'axios'
+import vueAxios from 'vue-axios'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+axios.defaults.withCredentials = true
 Vue.config.productionTip = false
+
+Vue.use(vueAxios, axios)
 
 new Vue({
   router,
