d486ab0123c624807e50a41f6fbf450dc72640d8
jiang6145 <jiang.notes14@gmail.com>
2021-01-12 15:56:19 +0800

    heartbeat 發出請求避免有效期限自動登出

--- cuishare/src/App.vue
+++ cuishare/src/App.vue
@@ -15,6 +15,35 @@ export default {
   name: 'App',
   components: {
     Navbar
+  },
+  computed: {
+    user () {
+      return this.$store.state.user
+    }
+  },
+  methods: {
+    async heartbeat () {
+      try {
+        const res = await this.axios.get(process.env.VUE_APP_API + '/users/heartbeat')
+
+        if (this.user.id && !res.data.success) {
+          alert('發生錯誤')
+          this.$store.commit('logout')
+          if (this.$route.path !== '/') this.$router.push('/')
+        }
+      } catch (error) {
+        alert(error.response.data.message)
+        this.$store.commit('logout')
+        if (this.$route.path !== '/') this.$router.push('/')
+      }
+    }
+  },
+  mounted () {
+    this.heartbeat()
+
+    setInterval(() => {
+      this.heartbeat()
+    }, 5000)
   }
 }
 </script>
