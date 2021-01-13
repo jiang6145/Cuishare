1bb54b4de1fa5730753c1c28cd6693e6ac0689bd
jiang6145 <jiang.notes14@gmail.com>
2021-01-13 13:38:32 +0800

    安裝 FontAwesomeVue

--- cuishare/src/main.js
+++ cuishare/src/main.js
@@ -4,11 +4,17 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import axios from 'axios'
 import vueAxios from 'vue-axios'
-
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import '../src/validation.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 axios.defaults.withCredentials = true
 Vue.config.productionTip = false
