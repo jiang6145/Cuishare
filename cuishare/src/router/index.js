f2f1da07ab06cb66eab1b94b09682542ca3b8aaf
jiang6145 <jiang.notes14@gmail.com>
2021-01-13 16:34:44 +0800

    vue router - Article

--- cuishare/src/router/index.js
+++ cuishare/src/router/index.js
@@ -13,15 +13,15 @@ const routes = [
       title: 'Cuishare | 首頁',
       isLogin: false
     }
+  },
+  {
+    path: '/article',
+    name: 'Article',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "article" */ '../views/Article.vue')
   }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
---
+++ cuishare/src/views/Article.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+#article
+  h1 文章頁面
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  mounted () {
+    console.log(this.$route.params)
+  }
+}
+</script>
