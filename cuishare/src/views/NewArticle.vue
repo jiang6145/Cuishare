c53d5e41d036ea51ef0887121e458488175d06d4
jiang6145 <jiang.notes14@gmail.com>
2021-01-14 16:35:19 +0800

    update

--- cuishare/src/views/NewArticle.vue
+++ cuishare/src/views/NewArticle.vue
@@ -1,13 +1,24 @@
 <template lang="pug">
 #new-article
-  h1 寫新文章
+  b-container
+    ckeditor#editor(:editor='editor' v-model='editorData' :config="editorConfig")
 </template>
 
 <script>
+import BalloonEditor from '@ckeditor/ckeditor5-build-balloon'
+
 export default {
   name: 'NewArticle',
-  mounted () {
-
+  data () {
+    return {
+      editor: BalloonEditor,
+      editorData: '<p>Content of the editor.</p>',
+      // 編輯器設定
+      editorConfig: {
+        // 工具列選項
+        toolbar: ['bold', 'italic', '|', 'link']
+      }
+    }
   }
 }
 </script>
