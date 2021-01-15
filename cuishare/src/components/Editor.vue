cbc7cf53b79a16cea7d3c25a8e05b5c66057e0e2
jiang6145 <jiang.notes14@gmail.com>
2021-01-15 16:02:27 +0800

    update

--- cuishare/src/components/Editor.vue
+++ cuishare/src/components/Editor.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
 #editor
   b-container
-    ckeditor(:editor="editor" v-model="editorData" :config="editorConfig")
+    ckeditor(:editor="editor" v-model="editorData" :config="editorConfig" @destroy="onEditorDestroy")
+    b-button(@click="onEditorSave()") 保存
 </template>
 
 <script>
@@ -9,38 +10,38 @@
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
 
 // Plugins
-import AutoImagePlugin from '@ckeditor/ckeditor5-image/src/autoimage'
-import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat'
-import AutolinkPlugin from '@ckeditor/ckeditor5-link/src/autolink'
-import AutosavePlugin from '@ckeditor/ckeditor5-autosave/src/autosave'
-import BlockQuotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote'
-import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold'
-import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials'
-import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading'
-import HighlightPlugin from '@ckeditor/ckeditor5-highlight/src/highlight'
-import HorizontalLinePlugin from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline'
-import ImagePlugin from '@ckeditor/ckeditor5-image/src/image'
-import ImageCaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption'
-import ImageInsertPlugin from '@ckeditor/ckeditor5-image/src/imageinsert'
-import ImageResizePlugin from '@ckeditor/ckeditor5-image/src/imageresize'
-import ImageStylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle'
-import ImageToolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar'
-import ImageUploadPlugin from '@ckeditor/ckeditor5-image/src/imageupload'
-import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic'
-import LinkPlugin from '@ckeditor/ckeditor5-link/src/link'
-import LinkImagePlugin from '@ckeditor/ckeditor5-link/src/linkimage'
-import ListPlugin from '@ckeditor/ckeditor5-list/src/list'
-import MediaEmbedPlugin from '@ckeditor/ckeditor5-media-embed/src/mediaembed'
-import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph'
-import PasteFromOfficePlugin from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice'
-import SimpleUploadAdapterPlugin from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter'
-import StrikethroughPlugin from '@ckeditor/ckeditor5-basic-styles/src/strikethrough'
-import TablePlugin from '@ckeditor/ckeditor5-table/src/table'
-import TableToolbarPlugin from '@ckeditor/ckeditor5-table/src/tabletoolbar'
-import TextTransformationPlugin from '@ckeditor/ckeditor5-typing/src/texttransformation'
-import TitlePlugin from '@ckeditor/ckeditor5-heading/src/title'
-import TodoListPlugin from '@ckeditor/ckeditor5-list/src/todolist'
-import WordCountPlugin from '@ckeditor/ckeditor5-word-count/src/wordcount'
+import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage'
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat'
+import Autolink from '@ckeditor/ckeditor5-link/src/autolink'
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave'
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
+import Heading from '@ckeditor/ckeditor5-heading/src/heading'
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight'
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline'
+import Image from '@ckeditor/ckeditor5-image/src/image'
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption'
+import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert'
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize'
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle'
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar'
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload'
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
+import Link from '@ckeditor/ckeditor5-link/src/link'
+import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage'
+import List from '@ckeditor/ckeditor5-list/src/list'
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed'
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice'
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter'
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough'
+import Table from '@ckeditor/ckeditor5-table/src/table'
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar'
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation'
+import Title from '@ckeditor/ckeditor5-heading/src/title'
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist'
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount'
 
 export default {
   name: 'Editor',
@@ -50,38 +51,38 @@ export default {
       editorData: null,
       editorConfig: {
         plugins: [
-          AutoImagePlugin,
-          AutoformatPlugin,
-          AutolinkPlugin,
-          AutosavePlugin,
-          BlockQuotePlugin,
-          BoldPlugin,
-          EssentialsPlugin,
-          HeadingPlugin,
-          HighlightPlugin,
-          HorizontalLinePlugin,
-          ImagePlugin,
-          ImageCaptionPlugin,
-          ImageInsertPlugin,
-          ImageResizePlugin,
-          ImageStylePlugin,
-          ImageToolbarPlugin,
-          ImageUploadPlugin,
-          ItalicPlugin,
-          LinkPlugin,
-          LinkImagePlugin,
-          ListPlugin,
-          MediaEmbedPlugin,
-          ParagraphPlugin,
-          PasteFromOfficePlugin,
-          SimpleUploadAdapterPlugin,
-          StrikethroughPlugin,
-          TablePlugin,
-          TableToolbarPlugin,
-          TextTransformationPlugin,
-          TitlePlugin,
-          TodoListPlugin,
-          WordCountPlugin
+          AutoImage,
+          Autoformat,
+          Autolink,
+          Autosave,
+          BlockQuote,
+          Bold,
+          Essentials,
+          Heading,
+          Highlight,
+          HorizontalLine,
+          Image,
+          ImageCaption,
+          ImageInsert,
+          ImageResize,
+          ImageStyle,
+          ImageToolbar,
+          ImageUpload,
+          Italic,
+          Link,
+          LinkImage,
+          List,
+          MediaEmbed,
+          Paragraph,
+          PasteFromOffice,
+          SimpleUploadAdapter,
+          Strikethrough,
+          Table,
+          TableToolbar,
+          TextTransformation,
+          Title,
+          TodoList,
+          WordCount
         ],
         toolbar: {
           items: [
@@ -114,6 +115,17 @@ export default {
         },
         placeholder: '寫下你的分享'
       }
+    }
+  },
+  methods: {
+    onEditorSave () {
+      // const editorData = this.editorData
+      // const parser = new DOMParser()
+      // const htmlDocument = parser.parseFromString(editorData, 'text/xml')
+      // const title = htmlDocument.querySelector('h1')
+    },
+    onEditorDestroy () {
+      console.log('Destroy')
     }
   }
 }
